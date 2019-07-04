7cc56ca9d8fe84806935dc1f49c7a8eed83c51f5
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-07-03 18:55:33 -0700

    scaffold

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './app.scss';
+import mockData from './mock.json';
+console.log(mockData)
+
+function App() {
+  return (
+   <>
+   <header>Task Master</header>
+   <main></main>
+   <footer></footer>
+   </>
+  );
+}
+
+export default App;
